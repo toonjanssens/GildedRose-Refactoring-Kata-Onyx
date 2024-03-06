65973d89204b72828a251e9bc7bee172275ee45c
Toon <toon@codibri.be>
2024-03-06 15:09:42 +0100

    remove code from more advanced refactor

--- TypeScript/app/base-rule.ts
+++
@@ -1,9 +0,0 @@
-import { Item, Rule } from "./types";
-
-export abstract class BaseRule implements Rule {
-  condition(_item: Item) {
-    return true;
-  }
-
-  abstract action(item: Item): Item;
-}
